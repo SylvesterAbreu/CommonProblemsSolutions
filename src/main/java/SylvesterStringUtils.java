32abb74714990364d62faae015ac537a0fd1ffa3
Sylvester Abreu <sylvester.abreu@digitaslbi.com>
2015-11-08 19:37:37 +0000

    Asserting that white spaces are removed from a string successfully

--- src/main/java/SylvesterStringUtils.java
+++ src/main/java/SylvesterStringUtils.java
@@ -6,4 +6,28 @@ public class SylvesterStringUtils {
         final StringBuilder stringReversed = reverser.reverse();
         return stringReversed.toString();
     }
+
+    public String removeWhiteSpaces(String original) {
+        StringBuilder noWhiteSpaces = new StringBuilder();
+        
+        for (int i =0;  i < original.length(); i++){
+            noWhiteSpaces = appendNonWhiteSpaces(original, noWhiteSpaces, i);
+        }
+        return noWhiteSpaces.toString();
+    }
+
+    private StringBuilder appendNonWhiteSpaces(String original, StringBuilder noWhiteSpaces, int i) {
+        final char characterAtCurrentPosition = original.charAt(i);
+        
+        if (hasNoSingleSpace(characterAtCurrentPosition)) {
+            noWhiteSpaces.append(characterAtCurrentPosition);
+        }
+        return noWhiteSpaces;
+    }
+
+    private boolean hasNoSingleSpace(char characterAtCurrentPosition) {
+        final char singleSpace = ' ';
+        
+        return singleSpace != characterAtCurrentPosition;
+    }
 }
--- src/test/java/SylvesterStringUtilsTest.java
+++ src/test/java/SylvesterStringUtilsTest.java
@@ -9,9 +9,20 @@ public class SylvesterStringUtilsTest {
     @Test
     public void thatStringPotatoesIsReversed() throws Exception {
         final String original = "potatoes";
+        
         final SylvesterStringUtils sylvesterStringUtils = new SylvesterStringUtils();
         final String stringReversed = sylvesterStringUtils.reverse(original);
         
         assertThat("seotatop", is(equalTo(stringReversed)));
+    }
+    
+    @Test
+    public void thatAllWhiteSpacesAreRemovedFromString() throws Exception {
+        final String original = "All White Spaces Are Removed From String";
+        
+        final SylvesterStringUtils sylvesterStringUtils = new SylvesterStringUtils();
+        final String noWhiteSpaces = sylvesterStringUtils.removeWhiteSpaces(original);
+        
+        assertThat("AllWhiteSpacesAreRemovedFromString", is(equalTo(noWhiteSpaces)));
     }
 }
