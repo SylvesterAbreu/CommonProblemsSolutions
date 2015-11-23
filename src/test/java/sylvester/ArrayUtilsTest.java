57a7cf3aaf4b23409cb5fc0a9d699b3bb809fe62
Sylvester Abreu <sylvester.abreu@digitaslbi.com>
2015-11-23 19:16:36 +0000

    Asserting that two different references referring to two different array objects with the same content are equal

---
+++ src/test/java/sylvester/ArrayUtilsTest.java
@@ -0,0 +1,22 @@
+package sylvester;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class ArrayUtilsTest {
+    
+    @Test
+    public void thatTwoArraysAreEqual() throws Exception {
+        final int [] arrayOne = new int[1];
+        arrayOne[0] = 2;
+
+        final int [] arrayTwo = new int[1];
+        arrayTwo[0] = 2;
+
+        assertThat(arrayOne, is(equalTo(arrayTwo)));
+    }
+}
